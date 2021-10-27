2a517dde05661a871e7938fa6dca1c9891c8bb83
Ingrid Inara <ingridinara@me.com>
2021-10-27 17:52:28 +0200

    create data folder with infor that will be mapped

---
+++ src/data/index.js
@@ -0,0 +1,255 @@
+export default {
+  nav: {
+    links: [
+      { textPath: ["Home", "/"] },
+      { textPath: ["Dev Tools", "/dev-lang-tools"] },
+      { textPath: ["Portfolio", "/portfolio"] },
+      { textPath: ["Education", "/education"] },
+      { textPath: ["Languages", "/languages"] },
+      { textPath: ["Social", "/social"] },
+    ],
+  },
+
+  about: {
+    text: ["hello there!", "I am a ", "web dev", ", based in Barcelona."],
+    typewriter: ["It's Ingrid Inara."],
+  },
+
+  devLangTools: {
+    title: ["Dev ", "languages & tools"],
+    tech: [
+      {
+        img: "https://github.com/ingridinara/pics/blob/main/1html5.svg?raw=true",
+        alt: "html5",
+        color: "#dd4b25",
+        level: "90%",
+      },
+      {
+        img: "https://github.com/ingridinara/pics/blob/main/1css3.svg?raw=true",
+        alt: "css",
+        color: "#006bb4",
+        level: "90%",
+      },
+      {
+        img: "https://github.com/ingridinara/pics/blob/main/1javascript.svg?raw=true",
+        alt: "javascript",
+        color: "#efd81d",
+        level: "80%",
+      },
+
+      {
+        img: "https://github.com/ingridinara/pics/blob/main/jquery.svg?raw=true",
+        alt: "jquery",
+        color: "#0868AC",
+        level: "80%",
+      },
+      {
+        img: "https://github.com/ingridinara/pics/blob/main/1Bootstrap.svg?raw=true",
+        alt: "bootstrap",
+        color: "#7710f1",
+        level: "90%",
+      },
+      {
+        img: "https://github.com/ingridinara/pics/blob/main/1sass.svg?raw=true",
+        alt: "sass",
+        color: "#c76494",
+        level: "70%",
+      },
+
+      {
+        img: "https://github.com/ingridinara/pics/blob/main/1materialui.svg?raw=true",
+        alt: "material ui",
+        color: "#0081cb",
+        level: "80%",
+      },
+      {
+        img: "https://github.com/ingridinara/pics/blob/main/1reactjs.svg?raw=true",
+        alt: "react js",
+        color: "#00d8ff",
+        level: "90%",
+      },
+
+      {
+        img: "https://github.com/ingridinara/pics/blob/main/1trello.svg?raw=true",
+        alt: "trello",
+        color: "#007ac3",
+        level: "90%",
+      },
+      {
+        img: "https://github.com/ingridinara/pics/blob/main/terminal.svg?raw=true",
+        alt: "terminal",
+        color: "#000000",
+        level: "40%",
+      },
+      {
+        img: "https://github.com/ingridinara/pics/blob/main/1git.svg?raw=true",
+        alt: "git",
+        color: "#e84e31",
+        level: "80%",
+      },
+      {
+        img: "https://github.com/ingridinara/pics/blob/main/1visual.svg?raw=true",
+        alt: "visual studio code",
+        color: "#0076c6",
+        level: "90%",
+      },
+      {
+        img: "https://github.com/ingridinara/pics/blob/main/typescript.svg?raw=true",
+        alt: "type script",
+        color: "#3178c6",
+        level: "60%",
+      },
+      {
+        img: "https://github.com/ingridinara/pics/blob/main/1wordpress.svg?raw=true",
+        alt: "wordpress",
+        color: "#006b8f",
+        level: "60%",
+      },
+
+      {
+        img: "https://github.com/ingridinara/pics/blob/main/figma.svg?raw=true",
+        alt: "figma",
+        color: "#e76e63",
+        level: "80%",
+      },
+
+      {
+        img: "https://github.com/ingridinara/pics/blob/main/nodejs.svg?raw=true",
+        alt: "node js",
+        color: "#77B063",
+        level: "40%",
+      },
+      {
+        img: "https://github.com/ingridinara/pics/blob/main/expressjs.svg?raw=true",
+        alt: "express",
+        color: "#000000",
+        level: "60%",
+      },
+      {
+        img: "https://github.com/ingridinara/pics/blob/main/mongodb.svg?raw=true",
+        alt: "mongo db",
+        color: "#12924f",
+        level: "40%",
+      },
+
+      {
+        img: "https://github.com/ingridinara/pics/blob/main/adobeXD.svg?raw=true",
+        alt: "adobe xd",
+        color: "#f75eee",
+        level: "80%",
+      },
+      {
+        img: "https://github.com/ingridinara/pics/blob/main/adobeIL.svg?raw=true",
+        alt: "adobe illustrator",
+        color: "#ff9400",
+        level: "70%",
+      },
+      {
+        img: "https://github.com/ingridinara/pics/blob/main/adobePSD.svg?raw=true",
+        alt: "adobe photoshop",
+        color: "#001d34",
+        level: "50%",
+      },
+    ],
+    text: [
+      "If you want to know more about the technologies and tools I' m working with on a daily basis, you just need to select any of the icons above. By doing that, you will be able to see the name and my current level of usage.",
+      " Check it out!",
+    ],
+  },
+
+  portfolio: {
+    title: "Portfolio",
+    subtitle:
+      "You could check some of my projects by clicking on the images below.",
+    projects: [
+      {
+        title: "Airbnb Clone (React JS)",
+        img: "https://github.com/ingridinara/pics/blob/main/set.jpg?raw=true",
+        alt: "photo of final overview of the Airbnb Clone App",
+        url: "https://github.com/ingridinara/pics/blob/main/set.jpg?raw=true",
+      },
+      {
+        title: "YouTube Clone (React JS)",
+        img: "https://github.com/ingridinara/pics/blob/main/set.jpg?raw=true",
+        alt: "photo of final overview of the YouTube Clone App",
+        url: "https://github.com/ingridinara/pics/blob/main/set.jpg?raw=true",
+      },
+      {
+        title: "NetFlix Clone (React JS)",
+        img: "https://github.com/ingridinara/pics/blob/main/set.jpg?raw=true",
+        alt: "photo of final overview of the NetFlix Clone App",
+        url: "https://github.com/ingridinara/pics/blob/main/set.jpg?raw=true",
+      },
+      {
+        title: "Weather App (React JS)",
+        img: "https://github.com/ingridinara/pics/blob/main/set.jpg?raw=true",
+        alt: "photo of final overview of the Weather App",
+        url: "https://github.com/ingridinara/pics/blob/main/set.jpg?raw=true",
+      },
+      {
+        title: "Maps App (React JS)",
+        img: "https://github.com/ingridinara/pics/blob/main/set.jpg?raw=true",
+        alt: "photo of final overview of the maps App",
+        url: "https://github.com/ingridinara/pics/blob/main/set.jpg?raw=true",
+      },
+    ],
+  },
+
+  studies: {
+    educationTitle: "Education",
+    education: [
+      {
+        school: "Papa Fam Member ",
+        degree: "Zero to Full Stack Hero",
+        graduated: "April 2021 - ongoing",
+      },
+      {
+        school: "IT Academy",
+        degree: "Front-end React JS",
+        graduated: "May 2020 - March 2021",
+      },
+      {
+        school: "Universidad de Barcelona",
+        degree: "Master on International Trade",
+        graduated: "Sep 2006 - Dic 2009",
+      },
+      {
+        school: "Universidade Católica do Salvador ",
+        degree: "Social Communication - PR ",
+        graduated: "Jan 2001 - Dic 2004",
+      },
+    ],
+  },
+
+  languages: {
+    title: "languages I speak",
+    details: [
+      {
+        name: "English",
+        level: "85%",
+      },
+      {
+        name: "Spanish",
+        level: "90%",
+      },
+      {
+        name: "Portuguese",
+        level: "99%",
+      },
+      {
+        name: "Italian",
+        level: "80%",
+      },
+    ],
+  },
+
+  social: {
+    text: "Template made with React JS by",
+    name: "Ingrid Inara",
+    url: [
+      "https://github.com/ingridinara?raw=true",
+      "https://www.linkedin.com/in/ingridinara/?raw=true",
+      "https://twitter.com/_IngridInara",
+    ],
+  },
+};
